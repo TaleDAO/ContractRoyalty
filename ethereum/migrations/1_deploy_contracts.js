cbd2419e4c1daad43ad7460b3af746ee16bd9eda
admin <admin@taledao.com>
2023-07-06 14:40:09 +0800

    fix

--- ethereum/migrations/1_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-const TaleDaoRoyalty = artifacts.require("TaleDaoRoyalty");
-
-module.exports = function(deployer) {
-
-  deployer.deploy(TaleDaoRoyalty, "v123456Z", ["0x01303af7f6f693af5889444ba7646ef7c4f9fe6d"], [100]);
-
-};
-
